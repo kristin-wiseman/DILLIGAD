72160fda93e209ace90a570685a0bb2a190311ae
Harini Sankar <hsankar3@illinois.edu>
2026-02-28 19:27:41 -0600

    Add duck biodata

---
+++ duck_info.js
@@ -0,0 +1,49 @@
+const DUCKS = [
+    {
+        "name": "Duckie",
+        "image": "images/base_duck.svg",
+        "bio": "This is Duckie. He loves wandering the wide wide world of the internet with his friends. Sometimes he gets lost and is grateful that you found him"
+    },
+    {
+        "name": "Flower Duckie",
+        "image": "images/flower_duck.svg",
+        "bio": "Duckie found a pretty flower in wild. He thinks it looks pretty as a hat."
+    },
+    {
+        "name": "Hat Duckie", 
+        "image": "image/hat_duck.svg", 
+        "bio": "Duckie loves showing off his favourite blue bucket hat. Yarn duck made it for him"
+    },
+    {
+        "name": "Hackstronaut Duck",
+        "image":"image/space_duck.svg",
+        "bio": "Space duck is on her journey exploring the depths of the HackAstra Universe. Its her first time too! She has had alot of fun hacking and learning how to code and even met an alien cat!"
+    },
+    {
+        "name": "Yarn Duck",
+        "image": "image/yarn_duck.svg",
+        "bio": "Yarn duck loves to find a quiet comfy corner to knit and crochet beautiful gifts for her friends! She even has an instagram page @yarning.for.u"
+    },
+    {
+        "name": "Stefan",
+        "image": "image/stabby_duck.svg",
+        "bio": "This is Stefan. Don't touch his computer screen or steal his White Monster drink...Unless you want to get stabbed."
+    },
+    {
+        "name": "Harini",
+        "image": "image/purple_duck.svg",
+        "bio": "This is Harini! She's one of the really cool people who made this extension. She's a cognitive psychologist by day and loves playing Stardew Valley. She really hopes this app has brought you joy!"
+    },
+    {
+        "name": "Kristin",
+        "image":"image/kristin_duck.svg",
+        "bio": "This is Kristin! She's one of the really cool people who made this extension. She is amazing!"
+    },
+    {"name": "Unknown",
+        "image": "image/unknown_duck.svg",
+        "bio": "You have not found this duck yet!",
+
+    }
+
+
+];
