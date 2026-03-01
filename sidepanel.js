f236d8a266f55fd1b9e2a7e68321ed0be5988114
Kristin Wiseman <88798340+kristin-wiseman@users.noreply.github.com>
2026-02-28 20:04:54 -0600

    Create .duck-grid inside barn panel.

--- sidepanel.js
+++ sidepanel.js
@@ -1,6 +1,54 @@
 
-// Tab behaviors
-document.addEventListener("DOMContentLoaded", () => {
+// Ducks packaged with extension
+const DUCKS = [
+  {
+    "name": "Duckie",
+    "image": "base_duck.svg",
+    "bio": "This is Duckie. He loves wandering the wide wide world of the internet with his friends. Sometimes he gets lost and is grateful that you found him"
+  },
+  {
+    "name": "Flower Duckie",
+    "image": "flower_duck.svg",
+    "bio": "Duckie found a pretty flower in wild. He thinks it looks pretty as a hat."
+  },
+  {
+    "name": "Hat Duckie",
+    "image": "hat_duck.svg",
+    "bio": "Duckie loves showing off his favourite blue bucket hat. Yarn duck made it for him"
+  },
+  {
+    "name": "Hackstronaut Duck",
+    "image": "space_duck.svg",
+    "bio": "Space duck is on her journey exploring the depths of the HackAstra Universe. Its her first time too! She has had alot of fun hacking and learning how to code and even met an alien cat!"
+  },
+  {
+    "name": "Yarn Duck",
+    "image": "yarn_duck.svg",
+    "bio": "Yarn duck loves to find a quiet comfy corner to knit and crochet beautiful gifts for her friends! She even has an instagram page @yarning.for.u"
+  },
+  {
+    "name": "Stefan",
+    "image": "stabby_duck.svg",
+    "bio": "This is Stefan. Don't touch his computer screen or steal his White Monster drink...Unless you want to get stabbed."
+  },
+  {
+    "name": "Harini",
+    "image": "purple_duck.svg",
+    "bio": "This is Harini! She's one of the really cool people who made this extension. She's a cognitive psychologist by day and loves playing Stardew Valley. She really hopes this app has brought you joy!"
+  },
+  {
+    "name": "Kristin",
+    "image": "kristin_duck.svg",
+    "bio": "This is Kristin! She's one of the really cool people who made this extension. She is amazing!"
+  },
+  {
+    "name": "Unknown",
+    "image": "unknown_duck.svg",
+    "bio": "You have not found this duck yet!",
+  }
+];
+
+function addTabBehaviors() {
   const tabs = Array.from(document.querySelectorAll('[role="tab"]'));
   const panels = Array.from(document.querySelectorAll('[role="tabpanel"]'));
 
@@ -42,4 +90,27 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+}
+
+function populateDuckGrid() {
+  const GRID = document.getElementById("duck-grid");
+  if (!GRID) return;
+
+  DUCKS.forEach(duck => {
+    const tile = document.createElement("div");
+    tile.className = "duck-tile";
+
+    const img = document.createElement("img");
+    img.src = `images/${duck.image}`;
+    img.alt = `${duck.name}`;
+
+    tile.appendChild(img);
+    GRID.appendChild(tile);
+  });
+}
+
+// Tab behaviors
+document.addEventListener("DOMContentLoaded", () => {
+  addTabBehaviors();
+  populateDuckGrid();
 });
